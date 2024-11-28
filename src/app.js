e793d4692e11eca1c850f7f1420ebc6f0b6d7d5a
Roriush <mwilliams.rorius@gmail.com>
2024-11-27 21:40:01 -0300

    test2

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 import express from "express";
-import cors from "cors";
+import cors from `cors`;
 
 const app = express();
 
+// Middlewares
 app.use(express.json());
 app.use(cors());
+
+app.get('/', (request, response) => {
+    const miMensaje = 'Mi primera api'
+    const users = 
+    response.status(200).json({ status: 200, message: "Respuesta exitosa", payload:"users" })
+    })
+
+    const PORT = 3000;
+    app.listen(PORT, () => {
+        console.log(`Servidor escuchando en el puerto ${PORT} ==> http://localhost: ${PORT}`);
+        });
